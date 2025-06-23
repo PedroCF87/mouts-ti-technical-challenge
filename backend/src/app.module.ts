07042d2192a8247af15dcbd35ed9f70aa824e53b
Pedro Figueira <pedro.figueira@suprema.group>
2025-06-23 10:37:11 -0300

    chore: add TypeORM configuration and setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { UserEntity } from './user.entity'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT ? +process.env.DB_PORT : 5432,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [UserEntity],
+        synchronize: false,
+      }),
+    }),
+    TypeOrmModule.forFeature([UserEntity]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
